d71aec293c060eb77fa58c709f871bbf347d0cb7
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2020-03-24 23:10:36 +0000

    smart contract developpement partial

--- contracts/DVSRegistry.sol
+++ contracts/DVSRegistry.sol
@@ -1,10 +1,62 @@
 pragma solidity >=0.5.13 <0.7.0;
 
+contract AddressList
+{
+    mapping (address => bool) public _addressMap;
+
+    function setWallet(address account) public {
+        _addressMap[account]=true;
+    }
+
+    function contains(address account) public view returns (bool){
+        return _addressMap[account];
+    }
+}
 contract DVSRegistry {
   string public message = 'Hello World';
+
+  mapping(string => address) private _ownerPerDocId;
+  mapping(string => string) private _encryptedKeyPerDocId;
+  mapping(string => mapping(address => bool)) private _authorizedAddressPerDocId;
+
 
   function setMessage(string calldata newMessage) external {
     message = newMessage;
   }
+
+  function docExists(string memory docId) public view returns(bool) {
+    address owner = _ownerPerDocId[docId];
+    return owner != address(0);
+  }
+
+  function registerDoc(string calldata docId, string calldata encryptedKey, address[] calldata authorizedAddresses) external {
+    require(!docExists(docId), "a document with this id already exists");
+    address owner = msg.sender;
+    _ownerPerDocId[docId] = owner;
+    _encryptedKeyPerDocId[docId] = encryptedKey;
+    for (uint i = 0; i < authorizedAddresses.length; i++) {
+      address account = authorizedAddresses[i];
+      _authorizedAddressPerDocId[docId][account] = true;
+    }
+  }
+
+  function getDocumentKey(string calldata docId) external view returns(string memory) {
+    require( (msg.sender == _ownerPerDocId[docId]) || _authorizedAddressPerDocId[docId][msg.sender], "not allowed to get the key for this document" );
+    return _encryptedKeyPerDocId[docId];
+  }
+
+  function setAccess(string memory docId, address[] memory authorizedAddresses, address[] memory deniedAddresses) public {
+    require(msg.sender == _ownerPerDocId[docId], "only the owner of the document can change authorisations");
+    for (uint i = 0; i < authorizedAddresses.length; i++) {
+      address account = authorizedAddresses[i];
+      _authorizedAddressPerDocId[docId][account] = true;
+    }
+    for (uint i = 0; i < deniedAddresses.length; i++) {
+      address account = deniedAddresses[i];
+      delete _authorizedAddressPerDocId[docId][account];
+    }
+  }
+
+
 
 }
--- test/DVSRegistry.spec.js
+++ test/DVSRegistry.spec.js
@@ -2,10 +2,24 @@ const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
 chai.use(chaiAsPromised).should();
 const { expect, assert } = chai
+const { v4: uuidv4 } = require('uuid');
 
 var DVSRegistry = artifacts.require('DVSRegistry');
 
-contract('Test DVSRegistry contract', function(accounts) {
+
+const ERROR_GENERIC = 'VM Exception while processing transaction\: revert';
+const REASON_GIVEN = '\-\- Reason given\:';
+
+function createExceptionMessage(reason, addReasonGiven = true) {
+    if (addReasonGiven) {
+        return new RegExp(ERROR_GENERIC + ' ' + reason + ' ' + REASON_GIVEN + ' ' + reason);
+    } else {
+        return new RegExp(ERROR_GENERIC + ' ' + reason);
+    }
+}
+
+
+contract('Fake test', function(accounts) {
     let dVSRegistry;
     it('connect DVSRegistry contract', async() => {
         dVSRegistry = await DVSRegistry.new();
@@ -15,5 +29,62 @@ contract('Test DVSRegistry contract', function(accounts) {
         await dVSRegistry.setMessage('Hello COVID-19');
         expect(await dVSRegistry.message()).to.not.eq('Hello World')
         expect(await dVSRegistry.message()).to.eq('Hello COVID-19')
+    })
+})
+
+
+contract('Test DVSRegistry contract', function(accounts) {
+    let dVSRegistry;
+    const doc1 = {
+        docId: uuidv4(),
+        encryptedKey: uuidv4(),
+        authorized: [],
+        subscriptionFee: 123456
+    }
+    const doc2 = {
+        docId: uuidv4(),
+        encryptedKey: uuidv4(),
+        authorized: [accounts[1], accounts[2]],
+        subscriptionFee: 0
+    }
+    const ERROR_NOT_ALLOWED_TO_GET_KEY = createExceptionMessage('not allowed to get the key for this document', false);
+
+    it('be able to register a protected document', async() => {
+        dVSRegistry = await DVSRegistry.new();
+        expect(await dVSRegistry.docExists(doc1.docId)).to.be.false;
+        expect(await dVSRegistry.docExists(doc2.docId)).to.be.false;
+        await dVSRegistry.registerDoc(doc1.docId, doc1.encryptedKey, doc1.authorized, { from: accounts[0] });
+        expect(await dVSRegistry.docExists(doc1.docId)).to.be.true;
+        expect(await dVSRegistry.docExists(doc2.docId)).to.be.false;
+    })
+    it('not be able to regsiter the same document again', async() => {
+        expect(false).to.be.true
+    })
+    it('be able to get encryption key if owner', async() => {
+        expect(await dVSRegistry.getDocumentKey(doc1.docId, { from: accounts[0] })).to.eq(doc1.encryptedKey);
+        await dVSRegistry.getDocumentKey(doc1.docId, { from: accounts[1] }).should.be.rejectedWith(ERROR_NOT_ALLOWED_TO_GET_KEY);
+    })
+    it('be able to get encryption key if authorized', async() => {
+        await dVSRegistry.registerDoc(doc2.docId, doc2.encryptedKey, doc2.authorized, { from: accounts[0] });
+        expect(await dVSRegistry.docExists(doc2.docId)).to.be.true;
+        expect(await dVSRegistry.getDocumentKey(doc2.docId, { from: accounts[0] })).to.eq(doc2.encryptedKey);
+        expect(await dVSRegistry.getDocumentKey(doc2.docId, { from: accounts[1] })).to.eq(doc2.encryptedKey);
+        expect(await dVSRegistry.getDocumentKey(doc2.docId, { from: accounts[2] })).to.eq(doc2.encryptedKey);
+        await dVSRegistry.getDocumentKey(doc2.docId, { from: accounts[3] }).should.be.rejectedWith(ERROR_NOT_ALLOWED_TO_GET_KEY);
+    })
+    it('be able to add authorized address to a document if owner', async() => {
+        await dVSRegistry.setAccess(doc1.docId, [accounts[1]], [], { from: accounts[0] });
+        expect(await dVSRegistry.getDocumentKey(doc1.docId, { from: accounts[0] })).to.eq(doc1.encryptedKey);
+        expect(await dVSRegistry.getDocumentKey(doc1.docId, { from: accounts[1] })).to.eq(doc1.encryptedKey);
+        await dVSRegistry.getDocumentKey(doc1.docId, { from: accounts[2] }).should.be.rejectedWith(ERROR_NOT_ALLOWED_TO_GET_KEY);
+    })
+    it('not be able to authorized address if not owner', async() => {
+        expect(false).to.be.true
+    })
+    it('be able to deny address if owner', async() => {
+        expect(false).to.be.true
+    })
+    it('not be able to deny address if not owner', async() => {
+        expect(false).to.be.true
     })
 })
